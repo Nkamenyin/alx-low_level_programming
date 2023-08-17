f4561e415773efde667d99afe72a6bfa828e34cd
Nkamenyin <valentineeyet01@gmail.com>
2023-08-17 11:08:21 -0700

    12

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,9 +6,15 @@
 void positive_or_negative(int i)
 {
 	if (i > 0)
+	{
 		printf("%d is positive\n", i);
+	}
 	if else (i > 0)
+	{
 		printf("%d is negative\n", i);
+	}
 	else
+	{
 		printf("%d is zero\n", i);
+	}
 }
