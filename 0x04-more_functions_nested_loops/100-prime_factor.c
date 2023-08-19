0d0177a1db33aa30ed3637b079c64100df9cfcb3
Nkamenyin <valentineeyet01@gmail.com>
2023-08-18 17:54:14 -0700

    finished

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,13 +1,25 @@
-#include "main.h"
+#include <stdio.h>
 /**
-* main - check the code
-* Return: Always 0.
+* main - entry point
+* Return: always 0 (sucess)
 */
 int main(void)
 {
-	print_triangle(2);
-	print_triangle(10);
-	print_triangle(1);
-	print_triangle(0);
+	unsigned long int num = 612852475143;
+	unsigned long int prim;
+
+	prim = 3;
+	while (prim < num / 2)
+	{
+		if ((num % prim) == 0)
+		{
+			if((prim % 3) == 2)
+				printf(",%lu ", prim);
+		}
+
+		prim+=2;
+	}
+
+	putchar('\n');
 	return (0);
 }
