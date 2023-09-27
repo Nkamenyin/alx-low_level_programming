a07ff33fa5ff3fed2476eae9142a87bfe0aa254d
Nkamenyin <valentineeyet01@gmail.com>
2023-09-27 05:57:34 +0100

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,11 +3,9 @@
 
 /**
  * first - it prints a sentence before main function
- * Return: 0
  */
 void first(void)
 {
 	printf("(You're beat! and yet, you must allow,)\n");
 	printf("(I bore my house upon my back!)\n");
-return (0)
 }
