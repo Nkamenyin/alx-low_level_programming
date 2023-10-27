e3f003377a6740a703a933a5e68570b54919dccd
Nkamenyin <valentineeyet01@gmail.com>
2023-10-27 03:57:52 +0100

    5

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ *@s: imput value
+Return: length of string
+*/
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(++s);
+	}
+	return (longit);
+}
