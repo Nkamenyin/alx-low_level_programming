4a02f4235afd59e41aa3bf91f377dd9af03a0d33
Nkamenyin <valentineeyet01@gmail.com>
2023-10-28 04:24:41 +0100

    12

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - it multiplies two numbers
+ *@argc: the arguargument count
+ *@argv: array of argumarguments
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		prinf("ERROR\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
