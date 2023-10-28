c23165bffff2669b47dc4bf45497af0f1dfd4712
Nkamenyin <valentineeyet01@gmail.com>
2023-10-28 04:00:27 +0100

    14

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - it prints all arguments it receives.
+ * main - it prints all the arguments it receives
  *@argc: the argument count
  *@argv: array of arguments
  *Return: 0
  */
- int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int a;
 
