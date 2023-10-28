55fb4efad1bfd4aba018d5a67de5a43fd0eb9661
Nkamenyin <valentineeyet01@gmail.com>
2023-10-28 02:48:25 +0100

    4

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - a program that prints its name, followed by a new line.
+ * main - a program that prints its name,
+ * followed by a new line.
  *@argc: the number of argument
  *@argv: the array of argument
  *Return: 0
