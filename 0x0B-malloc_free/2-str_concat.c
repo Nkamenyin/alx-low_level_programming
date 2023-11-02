704a84e9cbfd4fc69f132d3b90dd3937acfe6f6f
Nkamenyin <valentineeyet01@gmail.com>
2023-11-02 05:07:40 +0100

    7

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,6 +25,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (conct == NULL)
 		return (NULL);
+
 	i = j = 0;
 	while (s1[i] != '\0')
 	{
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,8 +3,8 @@
 /**
  * alloc_grid - returns a pointer to a 2 dimensional
  * array of integers
- *@ width: imput parameter
- *@ height: imput parameter
+ *@width: imput parameter
+ *@height: imput parameter
  * Return: pointer to 2 dimensional array
  */
 int **alloc_grid(int width, int height)
