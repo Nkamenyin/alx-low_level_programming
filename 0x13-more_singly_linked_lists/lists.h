ec1dc05a7b7b450760d1f1301cecbef878ccc257
Nkamenyin <valentineeyet01@gmail.com>
2023-11-21 23:00:52 +0100

    jop

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,12 +9,15 @@
  * @n: integer
  * @next: points to the next node
  */
+
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+}
+listint_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
