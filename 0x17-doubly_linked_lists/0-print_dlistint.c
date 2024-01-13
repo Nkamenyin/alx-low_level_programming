824207510fe0839469befc189954be55d48f7196
Nkamenyin <valentineeyet01@gmail.com>
2024-01-13 02:55:38 +0100

    dblists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include <list.h>
+
+/**
+ * print_dlistint - this prints all elements of dlistint_t list.
+ * @n:integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ * Return: node count
+ */
+typedef struct dlistint_s {
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h) {
+    size_t node_count = 0;
+
+    while (h != NULL) {
+        printf("%d", h->n);
+        node_count++;
+
+        h = h->next;
+        if (h != NULL) {
+            printf(" <-> ");
+	}
+    }
+    printf("\n");
+    return node_count;
+}
