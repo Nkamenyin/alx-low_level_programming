aea24ef53a6c5d5820d6037e0590e862fd4ecd75
Nkamenyin <valentineeyet01@gmail.com>
2024-02-09 22:07:04 +0100

    tble

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,36 +1,8 @@
 #include <hash_tables.h>
 
 /**
- * hash_node_t - Node of a hash table
- * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- */
-typedef struct hash_node_s
-{
-    char *key;
-    char *value;
-    struct hash_node_s *next;
-} hash_node_t;
-
-/**
- * hash_table_t - Hash table data structure
- * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
- */
-typedef struct hash_table_s
-{
-    unsigned long int size;
-    hash_node_t **array;
-} hash_table_t;
-
-/**
  * hash_table_create - Creates a hash table
  * @size: The size of the array
- *
  * Return: A pointer to the newly created hash table, or NULL if something went wrong
  */
 hash_table_t *hash_table_create(unsigned long int size)
@@ -39,11 +11,11 @@ hash_table_t *hash_table_create(unsigned long int size)
     unsigned long int i;
 
     if (size < 1)
-        return (NULL);
+	    return (NULL);
 
     new_table = malloc(sizeof(hash_table_t));
     if (new_table == NULL)
-        return (NULL);
+	    return (NULL);
 
     new_table->size = size;
 
@@ -53,9 +25,8 @@ hash_table_t *hash_table_create(unsigned long int size)
         free(new_table);
         return (NULL);
     }
-
     for (i = 0; i < size; i++)
-        new_table->array[i] = NULL;
+	    new_table->array[i] = NULL;
 
     return (new_table);
 }
